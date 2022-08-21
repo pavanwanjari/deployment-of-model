03c402fffb7d10eba1f93cef364ca3e45a64c465
Pavan Wanjari <92113558+pavanwanjari@users.noreply.github.com>
2022-08-22 00:37:37 +0530

    Update app.py

--- Stock-model-Deployment/app.py
+++ Stock-model-Deployment/app.py
@@ -1,6 +1,5 @@
-﻿import numpy as np
+import numpy as np
 import pandas as pd
-import matplotlib
 import matplotlib.pyplot as plt
 import pandas_datareader as data
 from keras.models import load_model
